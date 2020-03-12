52ffcf47359d2210c4bb297b166971d2d6ec97fc
AStoerkel1 <astoer1@students.towson.edu>
2020-03-12 16:26:15 -0400

    yes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import './nav.css';
 class Nav extends React.Component {
   render () {
     const navIcon = this.props.nav;
-    return <button className="navButton"><img src="src/icons8-usa-30.png" alt="flag"></img></button>
+    return <button className="navButton"><img src="icons8-usa-30.png" alt=""/></button>
   }
 }
 
